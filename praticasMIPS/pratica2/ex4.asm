d1f4d642f897dd8f79c53ba590c217e43907d294
Maria Fernanda Azolin <azolin.mf@gmail.com>
2019-09-29 19:43:08 -0300

    correcao

--- praticasMIPS/pratica2/ex4.asm
+++ praticasMIPS/pratica2/ex4.asm
@@ -1,6 +1,6 @@
 # Calcule a soma de todos os dados entre 0x1000 0000 e 0x1000 FFFC (inclusive).
 
-adiciona:	li $t0, 0x10000000
-		lw $t1, ($t0)
+		li $t0, 0x10000000
+adiciona:	lw $t1, ($t0)
 		addi $t0, $t0, 4
 		bne $t0, 0x10010000, adiciona
